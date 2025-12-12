2b8af06b402dfec20e06274de6d3ca25eb7378aa
tasbiha60 <htjtasbi@gmail.com>
2025-12-12 19:18:10 +0600

    Create pum.cpp
    codeforces

---
+++ pum.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+int num=1;
+for(int i=0;i<n;i++){
+    cout<<num<<" "<<num+1<<" "<<num+2<<" PUM\n";
+    num+=4;
+}
+return 0;
+
+
+
+}

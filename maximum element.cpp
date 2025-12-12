e7b878317519840414fc42f4bd6d3c829f930fa0
tasbiha60 <htjtasbi@gmail.com>
2025-12-12 08:21:14 +0600

    Create maximum element.cpp
    youkn0wwho.academy

---
+++ maximum element.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<long long>a(n);
+    for(int i=0;i<n;i++)
+            cin>>a[i];
+    cout<<*max_element(a.begin(),a.end());
+    return 0;
+}

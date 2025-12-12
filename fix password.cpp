c85801a6020df49824a0bd60fbadbc39f6cb2301
tasbiha60 <htjtasbi@gmail.com>
2025-12-12 18:44:32 +0600

    Create fix password.cpp
    codeforces

---
+++ fix password.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    while(cin>>x){
+        if(x==1999){
+            cout<<"Correct";
+            break;
+
+        }
+        else{
+            cout<<"Wrong\n";
+        }
+    }
+    return 0;
+}

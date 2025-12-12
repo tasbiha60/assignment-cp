c7277e304a5d2ff0fc8dfcd777b5795ebb2b31be
tasbiha60 <htjtasbi@gmail.com>
2025-12-12 21:32:46 +0600

    Create difference.cpp
    cf

---
+++ difference.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b,c,d;
+  cin>>a>>b>>c>>d;
+  int x=(a*b)-(c*d);
+  cout<<"Difference = "<<x<<endl;
+  return 0;
+
+
+    return 0;
+}
